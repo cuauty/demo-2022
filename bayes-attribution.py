11b378fb159e43970e612984288288b23ff1e229
yaxa <cuauty@users.noreply.github.com>
2022-11-21 14:25:27 +0800

    Update bayes-attribution.py

--- bayes-attribution.py
+++ bayes-attribution.py
@@ -2,7 +2,8 @@
 
 # These are the sample codes to perform the attribution using our bayesian network system. 
 # Because the system is used internally for now and necessary modules couldn't be imported, 
-# e.g. alps, the codes only explain Python interface of our system. We show GUI in the link
+# e.g. alps, the codes only explain Python interface of our system. In the conference we will
+# prepare the docker image. We also show GUI in the link
 # https://github.com/cuauty/demo-2022/blob/main/distri-bayes-demo.mp4?raw=true
 
 # Setup the sample dataset
